36b7aee2dc3e5c60259fea1db46cfb3d893ce310
VASARISHITHA <108422171+VASARISHITHA@users.noreply.github.com>
2023-10-27 19:18:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let buttons=document.getElementByTagName("button");
+let result=document.getElementById("result");
+for(i=0;i<buttons.length;i++)
+{
+buttons[i].onClick=function()
+{
+let input=this.innerText;
+updateDisplay(input)
+}
+}
+function updateDisplay(input)
+{
+let currentDisplay=result.innerText
+if(currentDisplay=="0")
+{
+if(input!="C"&&input!="DEL"&&input!="=")
+{
+//result.innerText=""
+//result.innerText=input
+}
+else{
+if(input=="DEL")
+{
+result.innerText=currentDisplay.substring(0,currentDisplay.length-1)
+if(result.innerText=="")
+{
+	result.innertext="0"
+}
+}
+if(input=="C")
+{
+result.innerText="0"
+}
+if(input!="C"&&input!="DEL"&&input!="=")
+{
+result.innerText+=input
+}
+if(input="=")
+{
+let display=result.innerText
+result.innerText=eval(display)
+}
+}
+}
